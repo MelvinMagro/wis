13c17788d7e8916c30ee510090e16ca59a635a4d
MelvinMagro <melvin.ko@hotmail.com>
2023-09-30 16:59:04 +0200

    Improved and decorated the site a bit for further testing

--- wp-content/themes/wistheme/front-page.php
+++ wp-content/themes/wistheme/front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
     <div class="content">
+        <a href="https://github.com/MelvinMagro/wis" target="_blank" class="github-link">Click here to go to the Github Repo</a>
     </div>
-
 
 <?php get_footer(); ?>
--- wp-content/themes/wistheme/header.php
+++ wp-content/themes/wistheme/header.php
@@ -10,5 +10,5 @@
 <body>
     
 <header> 
-
+MY WIS THEME
 </header>
